ff878059a723d2f950488dd55e49568937c4d6a5
Arvvv <thunderaarav100@gmail.com>
2026-07-18 18:59:05 +0530

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,9 +23,9 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "  ", // required by Discord API, not shown in the client
-        state: "You",     // this is what people actually see
-        type: 3,               // Custom
+        name: " Leaxsmp.fznetwork.fun:25578  ", // required by Discord API, not shown in the client
+        state: "",     // this is what people actually see
+        type: 0,               // Custom
       },
     ],
   },
